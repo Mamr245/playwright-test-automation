b220a5193cd0b0244eb6fddfd4358c991424b86a
Miguel <mamrodrigues24@gmail.com>
2025-12-04 15:43:03 +0000

    Removed Webkit project from playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,11 +35,6 @@ export default defineConfig({
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
     }
   ],
 });
